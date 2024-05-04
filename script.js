2e23b8e6acb48222603e40d554e66f7cb7f2e551
Nahuel2313 <168784069+Nahuel2313@users.noreply.github.com>
2024-05-04 00:27:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -18,3 +18,18 @@ function efectoHabilidades(){
 window.onscroll = function(){
     efectoHabilidades();
 } 
+
+
+//menu hamburguesa
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", ()=>{
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", ()=>{
+    nav.classList.remove("visible");
+})
